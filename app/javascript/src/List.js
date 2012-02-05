71fe98fc0f95fe34481cb825194c3733a9772aef
Mark Haskamp <mark.haskamp@pinnsg.com>
2012-02-05 11:37:47 -0500

    add_item

--- app/javascript/src/List.js
+++ app/javascript/src/List.js
@@ -1,5 +1,13 @@
 var List = {
 
   things: []
+
+  ,add_item: function(thing) {
+    this.things.unshift(thing);
+
+    if (this.things.length > 5) {
+      this.things.pop();
+    }
+  }
 };
 
--- test/spec/List_spec.js
+++ test/spec/List_spec.js
@@ -1,23 +1,38 @@
 describe("List", function() {
 
-  var fb;
-  beforeEach(function() {
-    list = new Object.beget(List);
-  });
-
-  afterEach(function() {
-    list = null;
-  });
-
   it ('starts with an empty list of "things"', function() {
+    var list = Object.beget(List);
     expect(list.things.length).toEqual(0);
   });
 
-  // describe("add_item", function() {
-  //   it("#add_item", function() {
-  //    expect(List.foo()).toEqual(42);
-  //   });
-  // });
+  describe("add_item", function() {
+
+    it("adds 'thing' to the list", function() {
+      var list = Object.beget(List);
+      list.add_item('foo');
+      expect(list.things[0]).toEqual('foo');
+    });
+
+    it("prepends 'thing' to the front of the list", function() {
+      var list = Object.beget(List);
+      list.add_item('foo');
+      list.add_item('bar');
+      expect(list.things[0]).toEqual('bar');
+    });
+
+    it("6th 'thing' pushes the last 'thing' off the list", function() {
+      var list = Object.beget(List);
+      list.add_item('1');
+      list.add_item('2');
+      list.add_item('3');
+      list.add_item('4');
+      list.add_item('5');
+      list.add_item('eddie would go');
+
+      expect(list.things[4]).toEqual('2');
+      expect(list.things.length).toEqual(5);
+    });
+  });
 });
 
 
