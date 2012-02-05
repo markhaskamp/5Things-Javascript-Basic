a3408fc3a62f1eacbb793fb461a80d61943f56dc
Mark Haskamp <mark.haskamp@pinnsg.com>
2012-02-05 11:39:17 -0500

    refactor

--- app/javascript/src/List.js
+++ app/javascript/src/List.js
@@ -2,7 +2,7 @@ var List = {
 
   things: []
 
-  ,add_item: function(thing) {
+  ,add_thing: function(thing) {
     this.things.unshift(thing);
 
     if (this.things.length > 5) {
--- test/spec/List_spec.js
+++ test/spec/List_spec.js
@@ -5,29 +5,29 @@ describe("List", function() {
     expect(list.things.length).toEqual(0);
   });
 
-  describe("add_item", function() {
+  describe("add_thing", function() {
 
     it("adds 'thing' to the list", function() {
       var list = Object.beget(List);
-      list.add_item('foo');
+      list.add_thing('foo');
       expect(list.things[0]).toEqual('foo');
     });
 
     it("prepends 'thing' to the front of the list", function() {
       var list = Object.beget(List);
-      list.add_item('foo');
-      list.add_item('bar');
+      list.add_thing('foo');
+      list.add_thing('bar');
       expect(list.things[0]).toEqual('bar');
     });
 
     it("6th 'thing' pushes the last 'thing' off the list", function() {
       var list = Object.beget(List);
-      list.add_item('1');
-      list.add_item('2');
-      list.add_item('3');
-      list.add_item('4');
-      list.add_item('5');
-      list.add_item('eddie would go');
+      list.add_thing('1');
+      list.add_thing('2');
+      list.add_thing('3');
+      list.add_thing('4');
+      list.add_thing('5');
+      list.add_thing('eddie would go');
 
       expect(list.things[4]).toEqual('2');
       expect(list.things.length).toEqual(5);
